997874def99c14f1cf65e8e34505849bd968d590
Marietka <Marietka@DESKTOP-KGUPT1P>
2021-03-19 19:29:34 +0200

    .

--- MyProject/src/my/MyClass1.java
+++ MyProject/src/my/MyClass1.java
@@ -1,7 +1,11 @@
-package my;
-
-public class MyClass1 {
-	public static void main(String[]args ) {
-System.out.println("Hello World");
+public class Main {
+	public static void main(String[] args ) {
+		displayInfo();
 }
+	private static void displayInfo() {
+		System.out.printIn("USP 2018 Example Project");
+		System.out.printIn("GitHub integration");
+		
+		System.out.printIn("Just new line");
+	}
 }
